8dbf47680eb6e16a3714f4e02dfea2f7b9ef95f6
Usedlobster <usedlobster@gmail.com>
2018-03-08 20:55:10 +0000

    Initalise PID values from command line

--- src/main.cpp
+++ src/main.cpp
@@ -28,12 +28,17 @@ std::string hasData(std::string s) {
   return "";
 }
 
-int main()
+int main( int argc , char *argv[] )
 {
   uWS::Hub h;
 
   PID pid;
-  // TODO: Initialize the pid variable.
+  // DONE: Initialize the pid variable.
+  pid.Init(  argc > 1 ? atof( argv[1] ) : 0.15 , 
+             argc > 2 ? atof( argv[2] ) : 0.0004, 
+             argc > 3 ? atof( argv[3] ) : 1.5  ) ;
+
+
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
